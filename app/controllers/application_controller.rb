38a19b81c970a11398c31a717af303f1792ef38f
Tnks <robsons@alunos.utfpr.edu.br>
2019-04-23 21:36:25 -0300

    Added model and crud operations for Condominium

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,29 @@
 class ApplicationController < ActionController::Base
-
+  
   layout :layout_by_resource
-
+  before_action if: :devise_controller?
+  
   protected
-
+  
   def layout_by_resource
-    return 'layouts/devise/session' if devise_controller?
-
-    'layouts/application'
+    case namespace_name
+    when 'devise'
+      'devise/layouts/application'
+    else
+      'layouts/application'
+    end
   end
-
+  
   def after_sign_in_path_for(*)
     admins_root_path
   end
-
+  
   def after_sign_out_path_for(*)
     new_admin_session_path
+  end
+  
+  def namespace_name
+    path = controller_path.split('/')
+    path.second ? path.first : nil
   end
 end
