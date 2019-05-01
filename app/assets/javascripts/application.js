56cb741f1844a9809c869afbaf814651b9fd77c4
Valdinei Moraes <valdineimoraes@alunos.utfpr.edu.br>
2019-05-01 10:03:59 -0300

    add imagens e mudanças na home

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,7 +7,7 @@
 //--------------------
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
+//= require bootstrap
 
 // Vendors
 //--------------------
@@ -16,6 +16,12 @@
 // Config files
 //--------------------
 //= require config/namespace
+//= require config/jquery.mask
+//= require config/simplemde
+//= require config/scrolling
+//= require config/gallery
+//= require config/navbar_collapse
+//= require config/scrollspy
 
 // App files
 //--------------------
